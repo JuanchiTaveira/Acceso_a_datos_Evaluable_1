b270a705b524bce55349d49a3e1b8c8a66599ac2
JuanchiTaveira <juanchitaveira@outlook.com>
2023-11-26 15:23:55 +0100

    read productos from URL ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            URL url = new URL("https://dummyjson.com/products");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error en la coneccion I/O");
+        }
     }
 }
